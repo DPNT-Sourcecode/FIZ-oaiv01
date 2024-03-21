0f58bc48860b482de81e79bd8eca200076966728
sbx_user1051 <sbx_user1051@169.254.35.165>
2024-03-21 11:49:43 +0000

    Thu Mar 21 11:49:43 UTC 2024

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -15,5 +15,17 @@ public class FizzBuzzSolution {
     	
        // throw new SolutionNotImplementedException();
     }
+    public String fizz_buzz(Integer number) {
+    	if((number%3==0) && number.toString().contains("3") && (number%5==0)&&number.toString().contains("5")) {
+    		return "fizz buzz";}
+    	else if((number%3==0) && number.toString().contains("3")) {
+    		return "fizz";
+    	}else if((number%5==0) && number.toString().contains("5")) {
+    		return "buzz";
+    	}else return "";
+       // throw new SolutionNotImplementedException();
+    }
+
 
 }
+
