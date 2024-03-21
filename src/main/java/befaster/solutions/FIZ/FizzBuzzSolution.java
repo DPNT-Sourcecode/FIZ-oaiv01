7b5a22686f88e03df8505b659f377dca421ffaea
sbx_user1051 <sbx_user1051@169.254.35.165>
2024-03-21 11:50:34 +0000

    Thu Mar 21 11:50:34 UTC 2024

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,14 +5,20 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-    	if((number%3==0) && (number%5)==0) {
+//    	if((number%3==0) && (number%5)==0) {
+//    		return "fizz buzz";}
+//    	else if(number%3==0) {
+//    		return "fizz";
+//    	}else if(number%5==0) {
+//    		return "buzz";
+//    	}else return number.toString();
+    	if((number%3==0) && number.toString().contains("3") && (number%5==0)&&number.toString().contains("5")) {
     		return "fizz buzz";}
-    	else if(number%3==0) {
+    	else if((number%3==0) && number.toString().contains("3")) {
     		return "fizz";
-    	}else if(number%5==0) {
+    	}else if((number%5==0) && number.toString().contains("5")) {
     		return "buzz";
-    	}else return number.toString();
-    	
+    	}else return "";
        // throw new SolutionNotImplementedException();
     }
     public String fizz_buzz(Integer number) {
@@ -28,4 +34,5 @@ public class FizzBuzzSolution {
 
 
 }
+
 
